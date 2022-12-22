d8e0379ce8b9d094326cb7f4fcee938195adff69
Vikas Gupta <vg733834@gmail.com>
2022-12-22 22:02:44 +0530

    -->1::10:2

---
+++ Sorting/Bubble_sort.cpp
@@ -0,0 +1,31 @@
+/*
+-> Time complexity in Worst Case is ----> O(N^2) 
+-> It swaps element n*(n-1) times
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of Array ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+    cin>>arr[i];
+    }
+    int counter = 1;
+    while(counter <= n-1){
+        for(int i=0;i<n-counter;i++){
+            if(arr[i]>arr[i+1]){
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+        counter++;
+    }
+cout<<"Sorted Array : ";
+    for(int i:arr){
+        cout<<i<<"  ";
+    }
+
+}
